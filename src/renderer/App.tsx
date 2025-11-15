879cec3273c2cf15e0eaef153d41f9c1aac28c23
manzygit <manzygithub@gmail.com>
2025-11-15 21:02:25 +0200

    modified:   package-lock.json modified:   package.json new file:   src/renderer/App.scss modified:   src/renderer/App.tsx

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,4 +1,5 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import './App.scss';
 
 export default function App() {
     useEffect(function(){
@@ -7,10 +8,23 @@ export default function App() {
             console.log(msg);
         });
     }, []);
+    
+    
+    let [count, setCount] = useState(0);
 
     return (
         <div className="app">
             <h1>Hello World!</h1>
+            <div className="count">
+                <span className="label">Balance: </span>
+                <span>${count}</span>
+            </div>
+            <button onClick={function(){
+                setCount(count + Math.round(Math.random() * 100));
+            }} className="gain">Gain Money</button>
+            <button onClick={function(){
+                setCount(0);
+            }} className="lose">Withdraw</button>
         </div>
     );
 }

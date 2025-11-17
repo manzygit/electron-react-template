c90255efe2aa6d1e537b0884f0f7132f0164fa63
manzygit <manzygithub@gmail.com>
2025-11-17 07:37:25 +0200

    modified:   src/main/main.ts

--- src/main/main.ts
+++ src/main/main.ts
@@ -4,7 +4,7 @@ import path from "path";
 const isDev = process.env.NODE_ENV === 'development' || process.env.VITE_DEV_SERVER_URL !== undefined;
 
 function createWindow() {
-    const win = new BrowserWindow({
+    let mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
@@ -14,19 +14,25 @@ function createWindow() {
         }
     });
     if(isDev){
-        win.loadURL('http://localhost:5173');
-        win.webContents.openDevTools();
+        mainWindow.loadURL('http://localhost:5173');
+        mainWindow.webContents.openDevTools();
         process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
     } else {
-        win.loadFile(path.resolve(
+        mainWindow.loadFile(path.resolve(
             app.getAppPath(), 'dist', "index.html"
         ));
+        mainWindow.removeMenu();
+        mainWindow.setMenuBarVisibility(false);
     }
-
-    ipcMain.on("sayHi", function(event) {
-        event.sender.send("hello", "Hello From Electron Backend!");
-    });
+    mainWindow.on("closed", function(){
+        mainWindow = null;
+    })
 }
+
+// IPC Handlers
+ipcMain.on("sayHi", function(event) {
+    event.sender.send("hello", "Hello From Electron Backend!");
+});
 
 app.whenReady().then(createWindow);
 
